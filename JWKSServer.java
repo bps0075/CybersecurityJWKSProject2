c0921c9609686d11f8a6305e4feefe6737dacf9a
bpsha <bps0075>
2023-09-22 17:47:57 -0500

    Added functionality for a RSA key pair.

--- JWKSServer.java
+++ JWKSServer.java
@@ -95,11 +95,24 @@ public class JWKSServer {
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+//import java.security.PrivateKey;
+//import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
+//import java.time.Instant;
+
+//import io.jsonwebtoken.Jwts;
+//import io.jsonwebtoken.SignatureAlgorithm;
+//import io.jsonwebtoken.security.Keys;
 
 public class JWKSServer {
+    //private static final String SECRET_KEY = "your-secret-key"; //Change this to your own secret key
     public static void main(String[] args) throws IOException {
         //This function is the first step to creating and authenticating the server
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
@@ -117,9 +130,38 @@ public class JWKSServer {
                 sendResponse(exchange, "Method Not Found", 405);
                 return;
             }
-            sendResponse(exchange, "{\"keys\":[]}", 200);
+            //sendResponse(exchange, "{\"keys\":[]}", 200);
+            KeyPair keyPair = generateRSAKeyPair(); //Generates an RSA key pair
+            String jwksResponse = buildJWKSResponse(keyPair); //Builds the JWKS JSON response
+            sendResponse(exchange, jwksResponse, 200); //Sends the JWKS response
+        }
+
+        private KeyPair generateRSAKeyPair() {
+            //Generates an RSA private key maybe (you get a public key for free)
+            try {
+                KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+                keyPairGenerator.initialize(2048); // You can adjust the key size as needed
+                return keyPairGenerator.generateKeyPair();
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+//            // Get the public and private keys from the key pair
+//            PublicKey publicKey = keyPair.getPublic();
+//            PrivateKey privateKey = keyPair.getPrivate();
+        }
+
+        private String buildJWKSResponse(KeyPair keyPair) {
+            //Formats the RSA public key required by JWKS
+            RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+            String modulus = Base64.getUrlEncoder().withoutPadding().encodeToString(publicKey.getModulus().toByteArray());
+            String exponent = Base64.getUrlEncoder().withoutPadding().encodeToString(publicKey.getPublicExponent().toByteArray());
+
+            return String.format("{\"keys\":[{\"kty\":\"RSA\",\"kid\":\"1\",\"n\":\"%s\",\"e\":\"%s\"}]}", modulus, exponent);
         }
     }
+
+    //final String goodKID = "aRandomKeyID";
 
     static class AuthHandler implements HttpHandler {
         //This function handles the http request POST
@@ -129,8 +171,23 @@ public class JWKSServer {
                 sendResponse(exchange, "Method Not Found", 405);
                 return;
             }
+            // Generate a JWT token with an expiration time
+//            String token = generateJWTWithExpiry();
+//            sendResponse(exchange, token, 200);
             sendResponse(exchange, "Authentication endpoint", 200);
         }
+
+//        private String generateJWTWithExpiry() {
+//            Instant now = Instant.now();
+//            Instant expirationTime = now.plusSeconds(3600); // Token expires in 1 hour
+//
+//            return Jwts.builder()
+//                    .setSubject("user123") // Set subject/username as needed
+//                    .setIssuedAt(java.util.Date.from(now))
+//                    .setExpiration(java.util.Date.from(expirationTime))
+//                    .signWith(SignatureAlgorithm.HS256, SECRET_KEY) // Change to your preferred algorithm and key
+//                    .compact();
+//        }
     }
 
     private static void sendResponse(HttpExchange exchange, String response, int statusCode) throws IOException {
@@ -143,7 +200,7 @@ public class JWKSServer {
     }
 }
 
-
+//Encodes your JWT
 
 /*let text = '{ "employees" : [' +
             '{ "firstName":"John" , "lastName":"Doe" },' +
